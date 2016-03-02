40d2509c67c9c26d0daef4642137483f121de4f9
vengojafar <jafar@vengolabs.com>
2016-03-02 17:09:44 -0500

    Init commit

---
+++ ExitCodeTest/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ExitCodeTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Process process = Process.Start("egalaxconsole.exe");
+
+            process.WaitForExit();
+
+            if (process.HasExited)
+            {
+                Console.WriteLine("Exit code: " + process.ExitCode);
+            }
+
+            Console.Read();
+        }
+    }
+}
